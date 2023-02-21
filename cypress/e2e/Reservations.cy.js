f73141df35ce568b80ddebfe36c76bae8dcd654f
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-02-21 12:16:00 -0600

    Intercept GET request in Cypress test

--- cypress/e2e/Reservations.cy.js
+++ cypress/e2e/Reservations.cy.js
@@ -1,12 +1,30 @@
+import reservations from '../fixtures/reservations.json'
+
 describe('Reservation App', () => {
   beforeEach(() => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: "http://localhost:3001/api/v1/reservations",
+      },
+      {
+        statusCode: 200,
+        body: reservations
+      }
+    );
+    
     cy.visit("http://localhost:3000");
+
   })
 
   it('Should Show Users all Reservations', () => {
-    cy.intercept("POST", `http://localhost:3001/add`, {
-      statusCode: 500,
-    });
+    
+    
+    
+  
+    cy.get('div')
+
+
 
 
 
