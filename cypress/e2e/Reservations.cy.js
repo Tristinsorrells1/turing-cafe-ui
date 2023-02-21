76d01ea9440e7777ff5a1f8a2f06b173a69424a7
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-02-21 12:32:21 -0600

    Write Cypress test to test if all reservations are displayed

--- cypress/e2e/Reservations.cy.js
+++ cypress/e2e/Reservations.cy.js
@@ -1,6 +1,6 @@
-import reservations from '../fixtures/reservations.json'
+import reservations from "../fixtures/reservations.json";
 
-describe('Reservation App', () => {
+describe("Reservation App", () => {
   beforeEach(() => {
     cy.intercept(
       {
@@ -9,24 +9,52 @@ describe('Reservation App', () => {
       },
       {
         statusCode: 200,
-        body: reservations
+        body: reservations,
       }
     );
-    
+
     cy.visit("http://localhost:3000");
+  });
 
-  })
+  it("Should Show Users all Reservations", () => {
+    cy.get('div[class="container"] > :nth-child(1) > h3 ').should(
+      "contain",
+      "Christie"
+    );
 
-  it('Should Show Users all Reservations', () => {
-    
-    
-    
-  
-    cy.get('div')
+    cy.get('div[class="container"] > :nth-child(1) > p:nth-of-type(1) ').should(
+      "contain",
+      "12/29"
+    );
 
+    cy.get('div[class="container"] > :nth-child(1) > p:nth-of-type(2) ').should(
+      "contain",
+      "7:00"
+    );
 
+    cy.get('div[class="container"] > :nth-child(1) > p:nth-of-type(3) ').should(
+      "contain",
+      "Number of guests: 12"
+    );
 
+    cy.get('div[class="container"] > :nth-child(2) > h3 ').should(
+      "contain",
+      "Leta"
+    );
 
+    cy.get('div[class="container"] > :nth-child(2) > p:nth-of-type(1) ').should(
+      "contain",
+      "4/5"
+    );
 
-  })
-})
+    cy.get('div[class="container"] > :nth-child(2) > p:nth-of-type(2) ').should(
+      "contain",
+      "7:00"
+    );
+
+    cy.get('div[class="container"] > :nth-child(2) > p:nth-of-type(3) ').should(
+      "contain",
+      "Number of guests: 2"
+    );
+  });
+});
